bb33d1e583fc93505c1c753d1425e2d7d77dc658
Akkshay <shindeakkshay@gmal.com>
2020-12-06 17:19:33 +0530

    Added endpoint to fetch all answers for particular question

--- quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
@@ -8,22 +8,28 @@ import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.InvalidQuestionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
 public class AnswerController {
+
     @Autowired
     private AnswerService answerService;
+
     /**
      * Create answer to a question
      *
-     * @param questionId : Question ID that you want to answer
-     * @param accessToken : access-token to authenticate
+     * @param questionId    : Question ID that you want to answer
+     * @param accessToken   : access-token to authenticate
      * @param answerRequest : The answer body
-     * @throws AuthorizationFailedException : Returns authorization failed exception
-     * @throws InvalidQuestionException : If question id is invalid returns invalid question response
      * @return answer creation resopnse
+     * @throws AuthorizationFailedException : Returns authorization failed exception
+     * @throws InvalidQuestionException     : If question id is invalid returns invalid question response
      */
     @PostMapping("/question/{questionId}/answer/create")
     public ResponseEntity<AnswerResponse> createAnswer(
@@ -44,11 +50,11 @@ public class AnswerController {
     /**
      * Update the answer
      *
-     * @param answerId : Answer ID that you want to Update
+     * @param answerId    : Answer ID that you want to Update
      * @param accessToken : access-token to authenticate
-     * @throws AuthorizationFailedException : Returns authorization failed exception
-     * @throws AnswerNotFoundException : If answer id is invalid returns invalid answer response
      * @return answer Updated response
+     * @throws AuthorizationFailedException : Returns authorization failed exception
+     * @throws AnswerNotFoundException      : If answer id is invalid returns invalid answer response
      */
     @PutMapping("/answer/edit/{answerId}")
     public ResponseEntity<AnswerEditResponse> editAnswer(
@@ -67,16 +73,13 @@ public class AnswerController {
     /**
      * Delete answer
      *
-     * @param answerId : Answer ID that you want to Delete
+     * @param answerId    : Answer ID that you want to Delete
      * @param accessToken : access-token to authenticate
-     * @throws AuthorizationFailedException : Returns authorization failed exception
-     * @throws AnswerNotFoundException : If answer id is invalid returns invalid answer id response
      * @return answer deleted response
+     * @throws AuthorizationFailedException : Returns authorization failed exception
+     * @throws AnswerNotFoundException      : If answer id is invalid returns invalid answer id response
      */
-    @RequestMapping(
-            method = RequestMethod.DELETE,
-            path = "/answer/delete/{answerId}",
-            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @DeleteMapping("/answer/delete/{answerId}")
     public ResponseEntity<AnswerDeleteResponse> deleteAnswer(
             @RequestHeader("authorization") final String accessToken,
             @PathVariable("answerId") String answerId)
@@ -86,4 +89,33 @@ public class AnswerController {
                 new AnswerDeleteResponse().id(answerEntity.getUuid()).status("ANSWER DELETED");
         return new ResponseEntity<AnswerDeleteResponse>(answerDeleteResponse, HttpStatus.OK);
     }
+
+    /**
+     * Fetch all the answer to a question
+     *
+     * @param questionId  : Answer ID that you want to Delete
+     * @param accessToken : access-token to authenticate
+     * @return All the answer to the Question
+     * @throws AuthorizationFailedException : Returns authorization failed exception
+     * @throws InvalidQuestionException     : If Question id is invalid returns invalid Question id
+     *                                      response
+     */
+    @GetMapping("/answer/all/{questionId}")
+    public ResponseEntity<List<AnswerDetailsResponse>> getAllAnswersToQuestion(
+            @RequestHeader("authorization") final String accessToken,
+            @PathVariable("questionId") String questionId)
+            throws AuthorizationFailedException, InvalidQuestionException {
+        List<AnswerEntity> answers = answerService.getAllAnswersToQuestion(questionId, accessToken);
+        List<AnswerDetailsResponse> answerDetailsResponses = new ArrayList<>();
+        for (AnswerEntity answerEntity : answers) {
+            AnswerDetailsResponse answerDetailsResponse = new AnswerDetailsResponse();
+            answerDetailsResponse.setId(answerEntity.getUuid());
+            answerDetailsResponse.setQuestionContent(answerEntity.getQuestionEntity().getContent());
+            answerDetailsResponse.setAnswerContent(answerEntity.getAnswer());
+            answerDetailsResponses.add(answerDetailsResponse);
+        }
+        return new ResponseEntity<List<AnswerDetailsResponse>>(answerDetailsResponses, HttpStatus.OK);
+    }
+
+
 }
--- quora-service/src/main/java/com/upgrad/quora/service/business/AnswerService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AnswerService.java
@@ -11,12 +11,15 @@ import com.upgrad.quora.service.exception.AnswerNotFoundException;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.InvalidQuestionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.UUID;
 
+@Service
 public class AnswerService {
 
     @Autowired
@@ -130,6 +133,33 @@ public class AnswerService {
             throw new AuthorizationFailedException(
                     "ATHR-003", "Only the answer owner or admin can delete the answer");
         }
+    }
+
+    /**
+     * Get all answer's
+     *
+     * @param questionId : questionid of which you want to see all answers
+     * @param accessToken : access-token for authentication
+     * @throws AuthorizationFailedException : if authentication is failed
+     * @throws InvalidQuestionException : if question id is invalid
+     * @return returns all the answers for a question
+     */
+    public List<AnswerEntity> getAllAnswersToQuestion(
+            final String questionId, final String accessToken)
+            throws AuthorizationFailedException, InvalidQuestionException {
+        UserAuthEntity userAuthEntity = authRepository.findByAccessToken(accessToken);
+        if (userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        } else if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException(
+                    "ATHR-002", "User is signed out.Sign in first to get the answers");
+        }
+        QuestionEntity questionEntity = questionRepository.findQuestionByUuid(questionId);
+        if (questionEntity == null) {
+            throw new InvalidQuestionException(
+                    "QUES-001", "The question with entered uuid whose details are to be seen does not exist");
+        }
+        return answerRepository.findAllAnswerByQuestionEntity(questionEntity);
     }
 
 }
